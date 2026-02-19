64eb7c49f5e2e99a353b6eb886084985791d6230
Luka <126253179+lukaCodes99@users.noreply.github.com>
2026-02-19 12:15:26 +0100

    removed api key for twilio

--- src/main/java/com/maletic/pacijentez/service/SmsService.java
+++ src/main/java/com/maletic/pacijentez/service/SmsService.java
@@ -9,15 +9,15 @@ import org.springframework.stereotype.Service;
 public class SmsService {
 
     // Twilio Credentials from Twilio dashboard
-    private static final String ACCOUNT_SID = "AC94055ba96e175c40a71c0e930b7dee43";
-    private static final String AUTH_TOKEN = "7027066ba20032abb6dd9e6fe560c9ec";
+    private static final String ACCOUNT_SID = "ACCOUNT_SID";
+    private static final String AUTH_TOKEN = "AUTH_TOKEN";
 
     static {
         Twilio.init(ACCOUNT_SID, AUTH_TOKEN);
     }
 
     public void sendSms(String body) {
-        String to = "+385993327725";
+        String to = "+broj";
         String poruka = "Lijepi pozdrav, " + body + "! "
                 + "Hvala što ste posjetili Polikliniku Maletić! Bili bismo Vam zahvalni ako biste podijelili svoje iskustvo ostavljanjem recenzije "
                 + "na sljedećoj poveznici: https://www.google.com/search?q=poliklinika+maletic"
